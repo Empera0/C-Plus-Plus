603433f9a3c2d020bc44939e473f566198e4a8d6
Oğuz <oguz.grncn@outlook.com>
2023-04-11 21:36:15 +0300

    RTTI

--- LambdaFunction/lambda.cpp
+++ LambdaFunction/lambda.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+    /*
+    Lambda function in C++ is more powerful than the ordinary function. Ordinary functions can only use
+    global variables or the variables passed to the function. Whereas lambda expressions can use local
+    variables present in the main method as well as parameters passed to the lambda expression along
+    with global variables
+    */
+
 int main(){
     int num1 = 100;
 
